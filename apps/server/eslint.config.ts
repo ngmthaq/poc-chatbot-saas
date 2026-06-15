894336e144b17d4f5c40277d2201194d1079d645
Victor Nguyen <nmthang6@cmcglobal.vn>
2026-06-15 15:53:01 +0700

    chore: update scripts

--- apps/server/eslint.config.ts
+++ apps/server/eslint.config.ts
@@ -5,6 +5,7 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig([
+  { ignores: ['dist'] },
   {
     files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
     plugins: { js },
